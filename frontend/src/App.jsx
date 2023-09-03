73e9993b89fbf5251b76c3cac6946857a31725df
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-03 16:57:26 -0700

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from "@apollo/client";
+
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  );
+}
