7c404681c6e823c98fa737c35f44ec94f671b22b
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-03 17:09:11 -0700

    Connected frontend to backend

--- frontend/graphql/client.js
+++ frontend/graphql/client.js
@@ -1,26 +1,9 @@
 import {
   ApolloClient,
   InMemoryCache,
-  gql,
 } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
-
-export const getAllProducts = async () => {
-  return client
-    .query({
-      query: gql`
-        query GetAllProducts {
-          products {
-            id
-            name
-            type
-          }
-        }
-      `,
-    })
-    .then((result) => console.log(result));
-};
---
+++ frontend/graphql/queries/GET_PRODUCTS.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const GET_PRODUCTS = gql`
+  query GetAllProducts {
+    products {
+      id
+      name
+      type
+    }
+  }
+`;
