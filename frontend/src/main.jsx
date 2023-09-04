db8ef4694d2ff411e97d0c14ee7943116f19d8b6
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-04 14:23:39 -0700

    Added react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from "@apollo/client";
 import { client } from "../graphql/client.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>
 );
