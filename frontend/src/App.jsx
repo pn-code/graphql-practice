c4aa38fd7d0d808f073657f3ad166a57c2b2c21f
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-03 17:09:48 -0700

    Added tailwind + cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-// Import everything needed to use the `useQuery` hook
-import { useQuery, gql } from "@apollo/client";
+import DisplayProducts from "../components/DisplayProducts";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen w-full">
       <h2>My first Apollo app 🚀</h2>
+      <DisplayProducts />
     </div>
   );
 }
