09450de5508ce022e408b56f0d23c5ce7a35fec2
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-04 16:05:44 -0700

    Created Reviews Display

---
+++ frontend/src/components/ReviewsDisplay.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReviewCard from "./ReviewCard";
+
+export default function ReviewsDisplay({ reviews }) {
+  return (
+    <section className="bg-gray-50 w-[300px] h-[260px] rounded-sm p-4">
+      <h2 className="font-semibold">Reviews</h2>
+
+      <div className="flex flex-col gap-2 overflow-y-auto h-full w-full">
+        {reviews.length > 0 ? reviews.map((review, idx) => (
+          <ReviewCard key={idx} review={review} />
+        )): <div className="w-full h-[80%] flex items-center justify-center font-semibold text-zinc-700">No reviews.</div>}
+      </div>
+    </section>
+  );
+}
