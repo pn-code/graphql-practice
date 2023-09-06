d720923eb22fa6f4b748999f512768c4cb0a5e59
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-06 12:09:06 -0700

    Added mobile styles to cart

--- frontend/src/routes/cart.jsx
+++ frontend/src/routes/cart.jsx
@@ -7,11 +7,12 @@ export default function Cart() {
   const { cart } = useContext(CartContext);
 
   const cartTotal = cart.reduce((acc, product) => product.price + acc, 0);
+
   return (
-    <section className="flex justify-center items-start sm:mt-10 sm:gap-10">
+    <section className="w-full min-h-[calc(100vh-200px)] flex flex-col p-2 sm:flex-row mt-2 gap-4 sm:justify-center sm:items-center sm:gap-10">
       <section className="flex flex-col gap-2">
         <h2 className="text-2xl font-semibold text-left">Your Cart</h2>
-        <section className="w-full h-[calc(100vh-200px)] flex flex-col gap-2 overflow-y-auto">
+        <section className="w-full h-[40vh] sm:h-[calc(100vh-200px)] flex flex-col gap-2 overflow-y-auto">
           {cart.length > 0 ? (
             cart.map((product, idx) => (
               <CartCard key={idx} index={idx} product={product} />
@@ -23,7 +24,7 @@ export default function Cart() {
           )}
         </section>
       </section>
-      <section className="flex flex-col gap-2">
+      <section className="flex flex-col gap-2 items-start justify-start">
         <h2 className="text-2xl font-semibold text-left">Your Total</h2>
         <div className="w-full sm:w-[200px] bg-gray-50 rounded-sm p-4 border-2 border-gray-500">
           <div className="text-lg font-semibold">
