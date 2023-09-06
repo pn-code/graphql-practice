8b72cb814dc7f7468eb3649ac6c31a943f7ca4db
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-06 12:09:39 -0700

    Updated height to remove white line at the bottom

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 export default function Root() {
   return (
-    <main className="flex flex-col h-screen w-full bg-gray-300">
+    <main className="flex flex-col min-h-[calc(101vh)] w-full bg-gray-300">
       <Navbar />
       <Outlet/>
     </main>
